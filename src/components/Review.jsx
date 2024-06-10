533e9e3d5913f020823d5d635f24ae4161bdb0b9
Anirudh Subbarao <asubbarao450@gmail.com>
2024-06-10 09:47:33 -0500

    added code for Review and made changes to store

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -4,24 +4,29 @@ import { useSelector, useDispatch } from 'react-redux';
 
 function Review(){
 
-const review = useSelector((store)=> store.review)
 
 const history = useHistory()
 
-const feelings = useSelector((store) => )
 
-const understanding =
+const feedback = useSelector(store => store.feedback)
 
-const support =
-
-const comments =
-
-const feedback = 
-
-
-   
+const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    axios.post(`/api/feedback`, feedback)
+      .then(response => {
+        
+        
+      })
+      .catch( error => {
+        console.log(error);
+        alert(`Sorry, couldn't add book at this time. Try again later`);
+      });
+  }
 
 }
+
+
 
 
 export default Review;
